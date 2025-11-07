9859591f876df7b4735034a9ea1c04a75be3ec67
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-07 18:05:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
-mod <dyn>;
-fn main() {}
+use cart::{Cart, CartBuilder, Component, Context};
+
+#[derive(Component)]
+struct Logger;
+
+impl Component for Logger {
+    fn start(&mut self, _ctx: &mut Context) -> Result<(), cart::CartError> {
+        println!("Logger started!");
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), cart::CartError> {
+    CartBuilder::builder()
+        .name("single-crate-cart")
+        .with_component(Logger)
+        .run()
+}
