07031c5bb05d34491bce4ddb13ef6048a6000933
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-08 01:22:03 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ pub mod core;
 pub mod errors;
 pub mod logger;
 pub mod prelude;
+pub mod attributes;
+
 pub use crate::core::*;
 pub use crate::errors::*;
 pub use crate::logger::*;
+pub use crate::attributes::*;
