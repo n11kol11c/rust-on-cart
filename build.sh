83c86d7dfe1baa4ef204568e8a30fba486bb8d57
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-26 22:51:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)   platform=linux;;
+    Darwin*)  platform=mac;;
+    CYGWIN*|MINGW*|MSYS*) platform=windows;;
+    *)       platform=unknown;;
+esac
+
+if [ "$platform" = "windows" ]; then
+    curl -sSf https://win.rustup.rs/x86_64 -o rustup-init.exe
+    ./rustup-init.exe -y
+    export PATH="$HOME/.cargo/bin:$PATH"
+else
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+fi
 
 rustup update
 cargo --version
