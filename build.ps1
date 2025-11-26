76d7411ba20e410d2ef999e727d6476056e16a04
м <174154109+n11kol11c@users.noreply.github.com>
2025-11-26 22:50:43 +0100

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$ProjectName = "",
+    [switch]$Build
+)
+
+Invoke-WebRequest -Uri "https://win.rustup.rs/x86_64" -OutFile "rustup-init.exe"
+Start-Process -FilePath ".\rustup-init.exe" -ArgumentList "-y" -Wait
+
+$env:Path += ";$env:USERPROFILE\.cargo\bin"
+$rustup = "$env:USERPROFILE\.cargo\bin\rustup.exe"
+
+& $rustup update
+cargo --version
+rustc --version
+
+if ($ProjectName -ne "") {
+    cargo new $ProjectName
+    Set-Location $ProjectName
+}
+
+if ($Build -and $ProjectName -ne "") {
+    cargo build
+}
